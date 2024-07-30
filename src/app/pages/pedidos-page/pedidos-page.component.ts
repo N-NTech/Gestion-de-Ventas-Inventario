122b9bee1ff00dfb192c55512feecf84b9252128
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2024-07-30 16:13:21 -0300

    Cambiar estado desde la tabla

--- src/app/pages/pedidos-page/pedidos-page.component.ts
+++ src/app/pages/pedidos-page/pedidos-page.component.ts
@@ -14,159 +14,173 @@ import { Estado } from './estado.enum';
 @Component({
     selector: 'table-filter-basic-demo',
     templateUrl: 'pedidos-page.component.html',
-    styleUrl:'pedidos-page.component.css', 
+    styleUrl: 'pedidos-page.component.css',
     standalone: true,
-    imports: [FormsModule,TableModule, TagModule, IconFieldModule, InputTextModule, InputIconModule, MultiSelectModule, DropdownModule, HttpClientModule, CommonModule]
+    imports: [FormsModule, TableModule, TagModule, IconFieldModule, InputTextModule, InputIconModule, MultiSelectModule, DropdownModule, HttpClientModule, CommonModule]
 })
 
 export class PedidosPageComponent implements OnInit {
 
-    
-  Estado: Estado | null = null;
-  estadoSeleccionado: Estado | null = null;
 
-  estadoOptions!: {label:any,value:any}[]
-  
-  pedidos =  [
-    {
-      id: 1,
-      nombre: 'Nicolás Arcamone',
-      estado: Estado.NuevoPedido,
-      producto: 'Nike- Jordan hight - Rosa y Negro - 45',
-      fecha: new Date(2024,4-1,11),
-      precio: '$23.000',
-      envio: 'Caballito'
-    },
-    {
-      id: 2,
-      nombre: 'María González',
-      estado: Estado.PagoPendiente,
-      producto: 'Adidas UltraBoost - Negro - 42',
-      fecha: new Date(2024,4-1,12),
-      precio: '$19.000',
-      envio: 'Palermo'
-    },
-    {
-      id: 3,
-      nombre: 'Juan Pérez',
-      estado: Estado.EnvioPendiente,
-      producto: 'Puma RS-X - Azul - 44',
-      fecha: new Date(2024,4-1,11),
-      precio: '$21.000',
-      envio: 'Belgrano'
-    },
-    {
-      id: 4,
-      nombre: 'Laura Martínez',
-      estado: Estado.Entregado,
-      producto: 'New Balance 574 - Gris - 40',
-      fecha: new Date(2024,4-1,14),
-      precio: '$25.000',
-      envio: 'Recoleta'
-    },
-    {
-      id: 5,
-      nombre: 'Carlos Fernández',
-      estado: Estado.Cancelado,
-      producto: 'Converse Chuck Taylor - Blanco - 43',
-      fecha: new Date(2024,4-1,15),
-      precio: '$15.000',
-      envio: 'San Telmo'
-    },
-    {
-        id: 6,
-        nombre: 'Carlos Fernández',
-        estado: Estado.RetiroPendiente,
-        producto: 'Converse Chuck Taylor - Blanco - 43',
-        fecha: new Date(2024,4-1,15),
-        precio: '$15.000',
-        envio: 'San Telmo'
-      },
-      {
-        id: 7,
-        nombre: 'Carlos Fernández',
-        estado: Estado.SinStock,
-        producto: 'Converse Chuck Taylor - Blanco - 43',
-        fecha: new Date(2024,4-1,15),
-        precio: '$15.000',
-        envio: 'San Telmo'
-      }
-  ];
+    Estado: Estado | null = null;
+    estadoSeleccionado: Estado | null = null;
 
-  
+    estadoOptions!: { label: any, value: any }[]
 
-  constructor() {
-    
-  }
+    pedidos = [
+        {
+            id: 1,
+            nombre: 'Nicolás Arcamone',
+            estado: Estado.NuevoPedido,
+            producto: 'Nike- Jordan hight - Rosa y Negro - 45',
+            fecha: new Date(2024, 4 - 1, 11),
+            precio: '$23.000',
+            envio: 'Caballito'
+        },
+        {
+            id: 2,
+            nombre: 'María González',
+            estado: Estado.PagoPendiente,
+            producto: 'Adidas UltraBoost - Negro - 42',
+            fecha: new Date(2024, 4 - 1, 12),
+            precio: '$19.000',
+            envio: 'Palermo'
+        },
+        {
+            id: 3,
+            nombre: 'Juan Pérez',
+            estado: Estado.EnvioPendiente,
+            producto: 'Puma RS-X - Azul - 44',
+            fecha: new Date(2024, 4 - 1, 11),
+            precio: '$21.000',
+            envio: 'Belgrano'
+        },
+        {
+            id: 4,
+            nombre: 'Laura Martínez',
+            estado: Estado.Entregado,
+            producto: 'New Balance 574 - Gris - 40',
+            fecha: new Date(2024, 4 - 1, 14),
+            precio: '$25.000',
+            envio: 'Recoleta'
+        },
+        {
+            id: 5,
+            nombre: 'Carlos Fernández',
+            estado: Estado.Cancelado,
+            producto: 'Converse Chuck Taylor - Blanco - 43',
+            fecha: new Date(2024, 4 - 1, 15),
+            precio: '$15.000',
+            envio: 'San Telmo'
+        },
+        {
+            id: 6,
+            nombre: 'Carlos Fernández',
+            estado: Estado.RetiroPendiente,
+            producto: 'Converse Chuck Taylor - Blanco - 43',
+            fecha: new Date(2024, 4 - 1, 15),
+            precio: '$15.000',
+            envio: 'San Telmo'
+        },
+        {
+            id: 7,
+            nombre: 'Carlos Fernández',
+            estado: Estado.SinStock,
+            producto: 'Converse Chuck Taylor - Blanco - 43',
+            fecha: new Date(2024, 4 - 1, 15),
+            precio: '$15.000',
+            envio: 'San Telmo'
+        }
+    ];
 
 
-  ngOnInit() {
-    this.estadoOptions = [
-              { label: 'Nuevo Pedido', value: Estado.NuevoPedido },
-              { label: 'Pago Pendiente', value: Estado.PagoPendiente },
-              { label: 'Retiro Pendiente', value: Estado.RetiroPendiente },
-              { label: 'Envío Pendiente', value: Estado.EnvioPendiente },
-              { label: 'Entregado', value: Estado.Entregado },
-              { label: 'Cancelado', value: Estado.Cancelado },
-              { label: 'Sin Stock', value: Estado.SinStock },
 
-          ]
-  }
+    constructor() {
 
-  getSeverity(estado: Estado): any | undefined {
-    switch (estado) {
-        case Estado.NuevoPedido:
-            return 'success';
-        case Estado.PagoPendiente:
-            return 'contrast';
-        case Estado.EnvioPendiente:
-            return 'warning';
-        case Estado.Entregado:
-            return 'primary';
-        case Estado.Cancelado:
-            return 'danger';
+    }
 
-        case Estado.RetiroPendiente:
-            return 'warning';
 
-        case Estado.SinStock:
-            return 'secondary';
+    ngOnInit() {
+        this.estadoOptions = [
+            { label: 'Nuevo Pedido', value: Estado.NuevoPedido },
+            { label: 'Pago Pendiente', value: Estado.PagoPendiente },
+            { label: 'Retiro Pendiente', value: Estado.RetiroPendiente },
+            { label: 'Envío Pendiente', value: Estado.EnvioPendiente },
+            { label: 'Entregado', value: Estado.Entregado },
+            { label: 'Cancelado', value: Estado.Cancelado },
+            { label: 'Sin Stock', value: Estado.SinStock },
 
-        default:
-            return undefined; 
+        ]
+    }
+
+    getSeverity(estado: Estado): any | undefined {
+        switch (estado) {
+            case Estado.NuevoPedido:
+                return 'success';
+            case Estado.PagoPendiente:
+                return 'contrast';
+            case Estado.EnvioPendiente:
+                return 'warning';
+            case Estado.Entregado:
+                return 'primary';
+            case Estado.Cancelado:
+                return 'danger';
+
+            case Estado.RetiroPendiente:
+                return 'warning';
+
+            case Estado.SinStock:
+                return 'secondary';
+
+            default:
+                return undefined;
         }
     }
 
     colorEstado(estado: Estado): any | undefined {
-      switch (estado) {
-        case Estado.NuevoPedido:
-            return '#014811';
-        case Estado.PagoPendiente:
-            return '#644200';
-        case Estado.EnvioPendiente:
-            return '#77791d';
-        case Estado.Entregado:
-            return '#005176';
-        case Estado.Cancelado:
-            return '#460000';
+        switch (estado) {
+            case Estado.NuevoPedido:
+                return '#014811';
+            case Estado.PagoPendiente:
+                return '#644200';
+            case Estado.EnvioPendiente:
+                return '#77791d';
+            case Estado.Entregado:
+                return '#005176';
+            case Estado.Cancelado:
+                return '#460000';
 
-        case Estado.RetiroPendiente:
-            return '#644200';
+            case Estado.RetiroPendiente:
+                return '#644200';
 
-        case Estado.SinStock:
-            return '#2d2d30';
+            case Estado.SinStock:
+                return '#2d2d30';
 
-        default:
-            return undefined; 
+            default:
+                return undefined;
         }
     }
-  
 
-    filter(event:any){
+    cambiarEstado(pedido: any) {
+        const estados = Object.values(Estado)
+        let seguir = true
+        estados.forEach(estado => {
+            if (pedido.estado == estado && seguir) {
+                let indexDelEstadoActual = estados.indexOf(pedido.estado)
+                pedido.estado = indexDelEstadoActual < estados.length - 1 ? estados[indexDelEstadoActual + 1] : estados[0]
+                seguir = false
+            }
+        })
+    }
+
+
+
+
+    filter(event: any) {
         console.log(event);
         return this.pedidos;
     }
 
-    
+
 
 }
